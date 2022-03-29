878c99e0999825312b9706330c6dcb33a1b25a89
Mike Thomsen <mikerthomsen@gmail.com>
2022-03-29 17:12:49 -0400

    Basic consumer/producer test.

---
+++ src/test/java/com/example/demokafkatest/services/StringKafkaProducer.java
@@ -0,0 +1,26 @@
+/**
+ * Derived from https://www.baeldung.com/spring-boot-kafka-testing
+ */
+
+package com.example.demokafkatest.services;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringKafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String payload) {
+        LOGGER.info("sending payload='{}' to topic='{}'", payload, topic);
+        kafkaTemplate.send(topic, payload);
+    }
+}
