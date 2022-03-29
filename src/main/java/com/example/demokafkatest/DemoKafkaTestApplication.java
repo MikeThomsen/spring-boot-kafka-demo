0d32c304fca5b6f58c068a388d47aefbc86143e8
Mike Thomsen <mikerthomsen@gmail.com>
2022-03-29 14:57:31 -0400

    Added kafka test starter.

---
+++ src/main/java/com/example/demokafkatest/DemoKafkaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKafkaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demokafkatest/DemoKafkaTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demokafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@SpringBootTest
+@EmbeddedKafka
+class DemoKafkaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
